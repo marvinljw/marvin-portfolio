a2060f880e51054247115ba178b91eedadb614ba
marvinljw <marvin.leow999@gmail.com>
2023-08-30 19:08:11 +0800

    Updates on skills

--- src/App.js
+++ src/App.js
@@ -33,12 +33,12 @@ function App() {
         setSelectedPage = {setSelectedPage} 
       />
       <div className="w-5/6 mx-auto md:h-full mb-12">
-        {isAboveMediumScreens && (
+        {/* {isAboveMediumScreens && (
           <DotGroup 
             selectedPage={selectedPage}
             setSelectedPage = {setSelectedPage} 
           />
-        )}
+        )} */}
         <Landing setSelectedPage={setSelectedPage} />
       </div>
 
--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -24,7 +24,10 @@ const MySkills = () => {
             MY <span className="text-red">SKILLS</span>
           </p>
           <LineGradient width="w-1/3" />
-          <p className="mt-10 mb-7">sadasdasd, asdasd asdasd</p>
+          <p className="mt-5 mb-7">
+            This is a very brief summary of skillsets that I have! Feel me to
+            ask me more :D
+          </p>
         </motion.div>
 
         <div className="mt-16 md:mt-0">
@@ -46,8 +49,8 @@ const MySkills = () => {
       </div>
 
       {/* SKILLS */}
-      <div className="skills h-[100%] md:flex md:justify-between gap-32">
-        {/* EXPERIENCE */}
+      <div className="skills h-[100%] md:flex md:justify-between gap-32 font-playfair">
+        {/* Programming Languages */}
         <motion.div
           className="md:w-1/3 mt-[8vh]"
           initial="hidden"
@@ -62,19 +65,18 @@ const MySkills = () => {
           <div className="relative">
             <div className="z-10">
               <p className="font-playfair font-semibold text-5xl">01</p>
-              <p className="font-playfair font-semibold text-3xl mt-3">
-                Experience
+              <p className="font-playfair font-semibold text-3xl mt-1">
+                Programming Languages
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
-            <p className="mt-[7vh]">
-              A auctor pharetra hendrerit mattis amet etiam interdum platea. Est
-              morbi porttitor scelerisque fermentum, sagittis non egestas. Amet
-              odio sit sagittis,
+            <div className="w-1/2 md:w-2/3 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+
+            <p className="mt-[5vh]">
+              <p> <span className="font-semibold text-blue">Development:</span> Python, Java, JavaScript, Elixir </p>
+              <p> <span className="font-semibold text-blue">Data-related:</span>  Python, R, MySQL, PostgreSQL</p>  
             </p>
           </div>
         </motion.div>
-
 
         {/* INNOVATIVE */}
         <motion.div
@@ -82,7 +84,7 @@ const MySkills = () => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay:0.2, duration: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, y: 50 },
             visible: { opacity: 1, y: 0 },
@@ -92,14 +94,15 @@ const MySkills = () => {
             <div className="z-10">
               <p className="font-playfair font-semibold text-5xl">02</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
-                Innovative
+                Web Development
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-2/3 h-32 bg-red absolute right-0 top-0 z-[-1]" />
             <p className="mt-[7vh]">
-              A auctor pharetra hendrerit mattis amet etiam interdum platea. Est
-              morbi porttitor scelerisque fermentum, sagittis non egestas. Amet
-              odio sit sagittis,
+            <p><span className="font-semibold text-red">Frontend:</span> ReactJS, VueJS, Tkinter, HTML, CSS, Phoenix Frontend</p>
+            <p><span className="font-semibold text-red">Backend:</span> Firebase, Phoenix Backend</p>
+            <p><span className="font-semibold text-red">Database:</span> MySQL</p>
+            <p><span className="font-semibold text-red">Others:</span> Agile(SCRUM), Confluence, Jiras, Git</p>
             </p>
           </div>
         </motion.div>
@@ -120,14 +123,14 @@ const MySkills = () => {
             <div className="z-10">
               <p className="font-playfair font-semibold text-5xl">03</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
-                Imaginative
+                Data-related
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-2/3 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
             <p className="mt-[7vh]">
-              A auctor pharetra hendrerit mattis amet etiam interdum platea. Est
-              morbi porttitor scelerisque fermentum, sagittis non egestas. Amet
-              odio sit sagittis,
+            <p><span className="font-semibold text-yellow">Date Visualisation:</span> Tableu, Excel </p>
+            <p><span className="font-semibold text-yellow">Machine Learning:</span> Linear/Multi-Regression, Convolutional Neural Network, Random Forest, Learning-to-Rank Model </p>
+
             </p>
           </div>
         </motion.div>
